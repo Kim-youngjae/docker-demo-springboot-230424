dd809565a7e679f61e009b67445c34346e142de1
youngjae459 <youngjae459@gmail.com>
2023-04-24 13:20:16 +0900

    프로젝트 생성

---
+++ src/main/java/com/ll/exam/app20230424/MainController.java
@@ -0,0 +1,15 @@
+package com.ll.exam.app20230424;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String root() {
+        return "Hello SpringBoot World!";
+    }
+}
